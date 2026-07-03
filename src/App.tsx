09c14dfc0c2fce22c172ad8928268a8734ef364c
Cameron Jensen <cameron.jensen93@gmail.com>
2026-07-03 15:55:27 -0700

    M2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { Model } from "./components/Model";
 import { Bounds, Html, OrbitControls, Environment } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Loader } from "./components/Loader";
 import { ErrorBoundary } from "react-error-boundary";
+import type { Vector3 } from "three";
 
 function App() {
   const url = "/models/triceratops_skull.glb";
+  const [points, setPoints] = useState<Vector3[]>([]);
+  const distance = points.length === 2 ? points[0].distanceTo(points[1]) : null;
 
   return (
     <>
@@ -31,12 +34,25 @@ function App() {
         >
           <Suspense fallback={<Loader />}>
             <Bounds fit clip observe>
-              <Model url={url} />
+              <Model url={url} points={points} setPoints={setPoints} />
             </Bounds>
-            <Environment background preset="city" />
+            <Environment preset="city" />
           </Suspense>
         </ErrorBoundary>
       </Canvas>
+      {distance !== null && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            color: "black",
+            pointerEvents: "none",
+          }}
+        >
+          <h1>Distance: {distance.toFixed(2)}</h1>
+        </div>
+      )}
     </>
   );
 }
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,11 +1,35 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Line } from "@react-three/drei";
+import type { ThreeEvent } from "@react-three/fiber";
+import type { Dispatch, SetStateAction } from "react";
+import { Vector3 } from "three";
 
 type ModelParams = {
   url: string;
+  points: Vector3[];
+  setPoints: Dispatch<SetStateAction<Vector3[]>>;
 };
 
-export const Model = ({ url }: ModelParams) => {
+export const Model = ({ url, points, setPoints }: ModelParams) => {
   const { scene } = useGLTF(url);
 
-  return <primitive object={scene} />;
+  const clickHandler = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
+    const point = e.point.clone();
+    setPoints((prev) => (prev.length === 2 ? [point] : [...prev, point]));
+  };
+
+  return (
+    <>
+      {points.map((v, i) => {
+        return (
+          <mesh scale={0.1} key={i} position={v}>
+            <sphereGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        );
+      })}
+      {points.length === 2 && <Line points={points} color={"red"} />}
+      <primitive onClick={clickHandler} object={scene} />
+    </>
+  );
 };
